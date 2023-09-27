7347d2c9338415a7e90c11e590c9799fe6e0ff14
edgars <ditfkultura@inbox.lv>
2023-09-27 13:53:20 +0300

    add send data

--- examples/scd4x_simpletest.py
+++ examples/scd4x_simpletest.py
@@ -1,10 +1,8 @@
-# SPDX-FileCopyrightText: 2020 by Bryan Siepert, written for Adafruit Industries
-#
-# SPDX-License-Identifier: Unlicense
 import time
 import board
 import adafruit_scd4x
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+from prometheus_client import Gauge, generate_latest, CollectorRegistry
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
@@ -15,6 +13,11 @@ scd4x.start_periodic_measurement()
 print("Waiting for first measurement....")
 
 app = Flask(__name__)
+
+# Create Prometheus metrics
+co2_gauge = Gauge('co2_ppm', 'CO2 concentration in ppm')
+temperature_gauge = Gauge('temperature_celsius', 'Temperature in Celsius')
+humidity_gauge = Gauge('relative_humidity_percentage', 'Relative Humidity in percentage')
 
 @app.route('/sensor-data', methods=['GET'])
 def get_sensor_data():
@@ -31,6 +34,19 @@ def get_sensor_data():
         return jsonify(data)
     else:
         return jsonify({'error': 'Sensor data not ready'}), 503
+
+@app.route('/metrics', methods=['GET'])
+def prometheus_metrics():
+    registry = CollectorRegistry()
+    co2_value = scd4x.CO2
+    temperature_value = scd4x.temperature
+    humidity_value = scd4x.relative_humidity
+
+    co2_gauge.set(co2_value)
+    temperature_gauge.set(temperature_value)
+    humidity_gauge.set(humidity_value)
+
+    return Response(generate_latest(registry), content_type='text/plain')
 
 if __name__ == '__main__':
     # Start the Flask app on port 5000
